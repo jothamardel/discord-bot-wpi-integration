c74d11f1b564a3f2bdd89d21036e7fc011b433cb
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2025-04-25 10:52:16 +0100

    update recipients for notify sommand

--- service/whatsapp.ts
+++ service/whatsapp.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as dotenv from 'dotenv';
+import { getParticipants } from './participants';
 dotenv.config();
 
 console.log('Token: ', process.env.WAAPI_TOKEN);
@@ -14,20 +15,12 @@ const axiosInstance = axios.create({
   baseURL: `https://waapi.app/api/v1/instances/${process.env.WAAPI_INSTANCE}/client/action/send-message`,
 });
 
+const recipients = getParticipants();
+console.log(recipients);
+
 export async function sendWhatsappMessage({
   message,
-  recipient = [
-    '2347033680280',
-    '2348064581905',
-    '2347089898766',
-    '2347074068911',
-    '2347060727649',
-    '2347062883534',
-    '2348065844484',
-    '2347036088996',
-    '2348069672917',
-    '2347018084869',
-  ],
+  recipient = recipients,
 }: {
   recipient?: string[];
   message: string;
